6dcfc7aed2981986cec1e0eba70f6a16647fa41a
rjbgoudie <rjbgoudie@users.noreply.github.com>
2026-03-10 08:14:52 +0000

    More detailed field-level summaries and better presentation

--- R/summarise.R
+++ R/summarise.R
@@ -1,3 +1,73 @@
+is_all_na_null_or_zero <- function(x, col, is_concept_id_col) {
+  if (is_concept_id_col) {
+    x |>
+      mutate(is_na = is.null(.data[[col]]) | .data[[col]] == 0L) |>
+      distinct(is_na) |>
+      pull(is_na) |>
+      all()
+  } else {
+    x |>
+      mutate(is_na = is.null(.data[[col]])) |>
+      distinct(is_na) |>
+      pull(is_na) |>
+      all()
+  }
+}
+
+is_all_na_null_or_zero <- function(x, col, is_concept_id_col) {
+  if (is_concept_id_col) {
+    x |>
+      summarise(is_na = any(!is.null(.data[[col]]) | .data[[col]] != 0L)) |>
+      pull(is_na)
+  } else {
+    x |>
+      mutate(is_na = is.null(.data[[col]])) |>
+      distinct(is_na) |>
+      pull(is_na) |>
+      all()
+  }
+}
+
+
+is_whole_column_na_null_or_zero <- function(x, col, is_concept_id_col) {
+  all_na <- x |>
+    head(1000) |>
+    is_all_na_null_or_zero({{ col }}, is_concept_id_col)
+  if (!all_na) {
+    FALSE
+  } else {
+    all_na <- x |>
+      is_all_na_null_or_zero({{ col }}, is_concept_id_col)
+
+    if (!all_na) {
+      FALSE
+    } else {
+      TRUE
+    }
+  }
+}
+
+
+n_distinct_concepts <- function(x, col) {
+  x |>
+    distinct(.data[[col]]) |>
+    count() |>
+    pull(n)
+}
+
+n_concept_zero <- function(x, col) {
+  x |>
+    filter(.data[[col]] == 0) |>
+    count() |>
+    pull(n)
+}
+
+concept_cross_tab <- function(x, col, concept_table) {
+  x |>
+    count(.data[[col]], sort = TRUE) |>
+    inner_join(concept_table, by = rlang::set_names("concept_id", col))
+}
+
 #' Summarise a single OMOP table
 #'
 #' @param table_name Name of the table
@@ -9,6 +79,8 @@ summarise_omop_table <- function(table_name, data, concept_table = NULL) {
   # Row Count
   cli::cli_progress_step("{table_name}: Calculating row count")
 
+  have_concept_table <- !is.null(concept_table)
+
   row_count <- data |>
     dplyr::tally() |>
     dplyr::collect() |>
@@ -16,71 +88,53 @@ summarise_omop_table <- function(table_name, data, concept_table = NULL) {
 
   # Column Summaries
   cols <- colnames(data)
+
   col_summary <- purrr::map_df(cols, function(col) {
-    cli::cli_progress_step("{table_name}.{col}: Checking for NAs")
-    # Check completeness (all 0, NA, or NULL)
-    null_count <- data |>
-      dplyr::filter(is.na(.data[[col]])) |>
-      dplyr::tally() |>
-      dplyr::collect() |>
-      dplyr::pull(n)
+    is_concept_id_col <- str_detect(col, "_concept_id")
 
-    # TODO handle concept 0
+    vocabs_used <- NA
+    n_distinct_concepts_counts <- NA
+    n_concept_zero_counts <- NA
+    cross_tab <- NA
+    if (have_concept_table && is_concept_id_col) {
+      cli::cli_progress_step("{table_name}.{col}: Forming cross-tab")
 
-    is_incomplete <- if (row_count == 0) FALSE else (null_count == row_count)
+      cross_tab <- data |>
+        concept_cross_tab(col, concept_table = concept_table) |>
+        collect()
 
+      cli::cli_progress_step("{table_name}.{col}: Checking vocabs")
+      vocabs_used <- cross_tab |>
+        distinct(vocabulary_id) |>
+        pull(vocabulary_id)
 
-    # Concept ID analysis
-    concept_info <- "N/A"
-    if (grepl("_concept_id$", col) && !is.null(concept_table)) {
-      cli::cli_progress_step("{table_name}.{col}: checking vocabs")
+      cli::cli_progress_step("{table_name}.{col}: Counting distinct concepts")
+      n_distinct_concepts_counts <- nrow(cross_tab)
 
-      vocabs <- data |>
-        dplyr::select(!!rlang::sym(col)) |>
-        dplyr::distinct() |>
-        dplyr::inner_join(concept_table, by = rlang::set_names("concept_id", col)) |>
-        dplyr::select(vocabulary_id) |>
-        dplyr::distinct() |>
-        dplyr::collect()
+      cli::cli_progress_step("{table_name}.{col}: Counting concept = 0")
+      n_concept_zero_counts <- cross_tab |>
+        filter(.data[[col]] == 0) |>
+        pull(n)
 
-      vocabs <- paste(unique(vocabs$vocabulary_id), collapse = ", ")
-      concept_info <- paste0("Vocabs: ", vocabs)
+      cli::cli_progress_step("{table_name}.{col}: Checking all NA or 0")
+      all_na_null_or_zero <- nrow(cross_tab) == 0 & all(cross_tab[[col]] == 0)
+    } else {
+      cli::cli_progress_step("{table_name}.{col}: Checking all NA or 0")
+      all_na_null_or_zero <- data |>
+        is_whole_column_na_null_or_zero(col, is_concept_id_col = is_concept_id_col)
     }
 
-    data.frame(
+    list(
+      table = table_name,
+      row_count = row_count,
       column = col,
-      incomplete = is_incomplete,
-      details = concept_info,
-      stringsAsFactors = FALSE
+      all_na_null_or_zero = all_na_null_or_zero,
+      vocabs_used = list(vocabs_used),
+      cross_tab = list(cross_tab),
+      n_distinct_concepts = n_distinct_concepts_counts,
+      n_concept_zero = n_concept_zero_counts
     )
   })
 
-  # Temporal Plots
-  plots <- list()
-  # date_cols <- cols[grepl('_date$|_datetime$', cols)]
-  #
-  # if (length(date_cols) > 0 && row_count > 0) {
-  #   date_col <- date_cols[1]
-  #   df_dates <- data |>
-  #     dplyr::select(!!rlang::sym(date_col)) |>
-  #     dplyr::collect() |>
-  #     dplyr::mutate(
-  #       date_val = as.Date(.data[[date_col]]),
-  #       year = lubridate::year(date_val),
-  #       dow = lubridate::wday(date_val, label = TRUE)
-  #     ) |>
-  #     dplyr::filter(!is.na(year))
-  #
-  #   if(nrow(df_dates) > 0) {
-  #     plots$year <- ggplot2::ggplot(df_dates, ggplot2::aes(x = year)) +
-  #       ggplot2::geom_bar(fill = '#2c3e50') + ggplot2::theme_minimal() +
-  #       ggplot2::labs(title = paste('Events per Year:', table_name))
-  #
-  #     plots$dow <- ggplot2::ggplot(df_dates, ggplot2::aes(x = dow)) +
-  #       ggplot2::geom_bar(fill = '#e74c3c') + ggplot2::theme_minimal() +
-  #       ggplot2::labs(title = 'Events by Day of Week')
-  #   }
-  # }
-
-  list(name = table_name, count = row_count, cols = col_summary, plots = plots)
+  bind_rows(col_summary)
 }
